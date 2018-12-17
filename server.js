729d1e98d943b54dab4541c25479d3496f168b8c
John Keller <jmkeller3@gmail.com>
2018-12-16 23:56:51 -0700

    added /

--- server.js
+++ server.js
@@ -34,10 +34,17 @@ passport.use(jwtStrategy);
 const jwtAuth = passport.authenticate("jwt", { session: false });
 
 // Use Routes
-app.use("api/users", users);
-app.use("api/reports", reports);
-app.use("api/restaurants", restaurants);
-app.use("api/auth", auth);
+app.use("//api/users", users);
+app.use("//api/reports", reports);
+app.use("//api/restaurants", restaurants);
+app.use("//api/auth", auth);
+
+// Protected Endpoint
+app.get("/api/protected", jwtAuth, (req, res) => {
+  return res.json({
+    data: "snoopy"
+  });
+});
 
 let server;
 
