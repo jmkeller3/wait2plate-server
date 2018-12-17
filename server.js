2cb3c08c99f6302e35a80b824de83b0885577ac8
John Keller <jmkeller3@gmail.com>
2018-12-16 23:54:04 -0700

    removed /

--- server.js
+++ server.js
@@ -34,17 +34,10 @@ passport.use(jwtStrategy);
 const jwtAuth = passport.authenticate("jwt", { session: false });
 
 // Use Routes
-app.use("/api/users", users);
-app.use("/api/reports", reports);
-app.use("/api/restaurants", restaurants);
-app.use("/api/auth", auth);
-
-// Protected Endpoint
-app.get("/api/protected", jwtAuth, (req, res) => {
-  return res.json({
-    data: "snoopy"
-  });
-});
+app.use("api/users", users);
+app.use("api/reports", reports);
+app.use("api/restaurants", restaurants);
+app.use("api/auth", auth);
 
 let server;
 
