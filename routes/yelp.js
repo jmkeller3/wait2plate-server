bea6f49b61556e3efe86199656c33598452954e4
John Keller <jmkeller3@gmail.com>
2019-01-03 14:15:07 -0700

    fixed yelp api issue with geolocation vs. location

--- routes/yelp.js
+++ routes/yelp.js
@@ -7,33 +7,61 @@ const client = yelp.client(
 // const longitude = -111.87851189999999;
 
 async function yelpAPI(latitude, longitude, location) {
-  try {
-    const data = await client.search({
-      latitude,
-      longitude,
-      location,
-      open_now: true,
-      sort_by: "distance"
-    });
+  if (!location) {
+    try {
+      const data = await client.search({
+        latitude,
+        longitude,
 
-    const businesses = JSON.parse(data.body).businesses;
-    const prettyData = businesses.map(business => {
-      let result = {
-        id: business.id,
-        name: business.name,
-        url: business.url,
-        image_url: business.image_url,
-        display_address: business.location.display_address,
-        display_phone: business.display_phone,
-        rating: business.rating,
-        distance: business.distance
-      };
-      return result;
-    });
-    return prettyData;
-  } catch (error) {
-    console.log(error);
-    throw error;
+        open_now: true,
+        sort_by: "distance"
+      });
+
+      const businesses = JSON.parse(data.body).businesses;
+      const prettyData = businesses.map(business => {
+        let result = {
+          id: business.id,
+          name: business.name,
+          url: business.url,
+          image_url: business.image_url,
+          display_address: business.location.display_address,
+          display_phone: business.display_phone,
+          rating: business.rating,
+          distance: business.distance
+        };
+        return result;
+      });
+      return prettyData;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  } else {
+    try {
+      const data = await client.search({
+        location,
+        open_now: true,
+        sort_by: "distance"
+      });
+
+      const businesses = JSON.parse(data.body).businesses;
+      const prettyData = businesses.map(business => {
+        let result = {
+          id: business.id,
+          name: business.name,
+          url: business.url,
+          image_url: business.image_url,
+          display_address: business.location.display_address,
+          display_phone: business.display_phone,
+          rating: business.rating,
+          distance: business.distance
+        };
+        return result;
+      });
+      return prettyData;
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
